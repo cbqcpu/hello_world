b716e4cba43d15e52f14274b8c5836021ea41216
cpu <cpu@DESKTOP-K7TVJIH>
2017-08-02 10:09:32 +0800

    Add project files.

---
+++ hello_world/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* my first C# program */
+            Console.WriteLine("Hello World!");
+
+            char testType = new char();
+            if (testType.GetType().IsValueType)
+            {
+                Console.WriteLine("{0} is value type.", testType.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
