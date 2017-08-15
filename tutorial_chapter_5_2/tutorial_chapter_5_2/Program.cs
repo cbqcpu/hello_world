ec0e11bd3daf50e73359a5e549ed4a9567f12930
cpu <cpu@DESKTOP-K7TVJIH>
2017-08-15 16:22:37 +0800

    2017.8.15_OOA

---
+++ tutorial_chapter_5_2/tutorial_chapter_5_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial_chapter_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rain rain = new Rain();
+            Sun sun = new Sun();
+            rain.Print();
+            sun.Print();
+            Console.ReadLine();
+
+            Son son = new Son();
+            son.Name();
+            ((Father)son).Name();
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Wether
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("天气为：");
+        }
+    }
+
+    public class Rain : Wether
+    {
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("雨");
+        }
+
+    }
+    public class Sun : Wether
+    {
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("晴");
+        }
+    }
+    //用new关键字隐藏基类成员
+    public class Father
+    {
+        public void Name()
+        {
+            Console.WriteLine("爸爸叫大明");
+        }
+    }
+
+    public class Son : Father
+    {
+        public new void Name()
+        {
+            Console.WriteLine("儿子叫小明");
+        }
+    }
+
+
+
+}
